0b15b27a931dd11d7c5c03a56bdaaf2cc15bafa1
Kait <kait.mikitin@gmail.com>
2018-04-19 12:15:23 -0500

    updated views.py in polls, need to add results template

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+# This helps with namespacing
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 # with render you don't need HttpResponse or loader
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Question
 
@@ -17,8 +18,23 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on questions %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.post lets you access submitted data by key name
+        # w/choice returns the ID of the selected choice as a string
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+    # ALWAYS RETURN A REDIRECT when working with post data
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
